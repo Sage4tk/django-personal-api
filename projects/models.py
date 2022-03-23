866c1787e04044985958bc4b95067613f2756156
timothy timbol <ttimbol08@gmail.com>
2022-03-23 11:49:41 +0400

    added project model to admin, added requireqs

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=255)
+    github_link = models.CharField(max_length=255)
+    technology = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField('date published')
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from projects.models import Project
+
 # Create your views here.
 def index(req):
-    data={
-        "users":[{"name":"zack"},{"name":"zack"},]
-    }
-    return JsonResponse(data)
+    data = list(Project.objects.values())
+    return JsonResponse(data, safe=False)
